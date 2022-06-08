1fc1498e78f0b2e6df3d02023b025a73da940136
dakforz <102936873+dakforz@users.noreply.github.com>
2022-06-08 18:08:28 -0400

    Update 1-count_kmers.py

--- 1-count_kmers.py
+++ 1-count_kmers.py
@@ -1,63 +1,78 @@
+# -*- coding: utf-8 -*-
 """
-Updated on 2022-04-02
+Created on Sat Apr 30 23:50:48 2022
 
-@author: Dakforz Chang 
+@author: dakforz
+
+This program will convert sequences into kmers
+
+Details: 
+1.read any fasta or fastq file
+2.break all the sequences into your desired kmer size 
+3.count how many times each kmer appears in the file of sequencing read
+4.create a table into result.txt that have all the kmer seqeunce and it's number of appearance
+
+#sys.argv[1] input with filename (ex.sequence.fastq)
+#sys.argv[2] input with interested kmer size (ex.17)
+#sys.argv[3] input with filetype (fastq)
+
+
+
 """
-
 
 from Bio import SeqIO
-import sys #since this program use spyder, the argument is stored in run-configuration per file function
+import sys
 
-try:
- seqFile = open(sys.argv[1],"r")  #try for first argument - sequences.fastq
- seqIOObject = SeqIO.parse(seqFile, sys.argv[3]) #try for third argument - fastq 
- 
+try:  #error handling for file's name and type 
+    readFile = open(sys.argv[1],"r")   
+    seqIOObject = SeqIO.parse(readFile, sys.argv[3])
+    
 except IOError:
- print("Could not open the file!") #confirm first argument is exist
- sys.exit()
+    print("Can not open the file, Please re-check your spelling.")
+    sys.exit()
 
 except ValueError:
- print("The file should be fasta or fastq!") #if the file type is wrong, it will pop out 
- sys.exit()
+    print("The filetype should be in .fasta or .fastq.") 
+    sys.exit()
  
-try: 
- kmer = int(sys.argv[2]) #try second argument -17 here, if it is not a integar number the error will show
+try:  #error handling for kmer size's number
+    kmer = int(sys.argv[2]) 
 except ValueError:
- print("The kmers should be an integar number!")
- sys.exit()
+    print("The kmer size should be an integar number.")
+    sys.exit()
 
-kmers = [] #this store every kmer sequence (ex.ATCG TCGA..)
-kmersnum = [] #this store every sequence repeat number (they are follow by kmer's index)
 
-Filehandle= open("result.txt","w") #write the following graph in result file
-space = " " #help with formating 
-Filehandle.writelines("%s %s %s\n" % ("sequence(s)",space*(kmer-4),"count(s)")) #this is the title of the graph;space*(kmer-4) will locate in the center  
 
-for record in seqIOObject: #ths read each line of sequences.fastq and then anaylze each line
-    seq =record.seq #only get the sequence information
-    n= 0 #ths is used for tracking kmer's number (n-k+1)
+#kmers and kmers_counts shared the same index
+kmers = []  #this will only store the kmer information
+kmers_counts = [] #this will store the counts information
+
+writeFile= open("result.txt","w")  #write the results into result.txt file 
+space = " "  #this is for formating the file only
+writeFile.writelines("%s %s %s\n" % ("sequence(s)",space*(kmer-4),"count(s)")) 
+
+for record in seqIOObject:
+    seq =record.seq #only getting sequence information
+    n= 0  #this is for tracking kmer's number (n-k+1)
   
-    while kmer+n <= int(len(seq)):  #this make sure each kmers do not exceed its assign number
-        newstring = seq[0+n:kmer+n] #this specify the sequences's kmer using the range
-        #print(kmer+n, len(seq),newstring)
-        if newstring in kmers:  #this check for kmer replication
-            location = int(kmers.index(newstring)) #this check the replication index location
-            existingnum = kmersnum[location] #this read the existing replication number (since it may be larger than 1)
-            newnum = int(existingnum) + 1 #the kmer number will add value 1 
-            kmersnum[location] = newnum  #store/replace the above number into list 
+    while kmer+n <= int(len(seq)):  
+        newstring = seq[0+n:kmer+n]
+        
+        if newstring in kmers:  #if the kmer has already existed, it will increase the counts
+            location = int(kmers.index(newstring))
+            existing_counts = kmers_counts[location] 
+            new_counts = int(existing_counts) + 1 
+            kmers_counts[location] = new_counts  
             
-        else: #if kmer do not replicate(exist before) it will write the kmer sequence in list, and assign value 1 to another list (first appearance)
+        else:
             kmers.append(newstring)
-            kmersnum.append('1')
-        n += 1 #this will make sure our kmer push left
+            kmers_counts.append('1')
+        n += 1 
     
        
-for writein in range(len(kmers)): #this line just write the list into file 
-     Filehandle.writelines("%s %s %s\n" % (kmers[writein],space*(kmer-8),kmersnum[writein]))
+for index in range(len(kmers)): 
+     writeFile.writelines("%s\t %s\n" % (kmers[index],kmers_counts[index]))
 
 
-seqFile.close() #close the references sequence file 
-Filehandle.close() #close the graph file
-       
-  
-    
+readFile.close() 
+writeFile.close() 
